63b3e052b06725cda3661590e0184d0ddc03c7c2
Thomas Robitaille <thomas.robitaille@gmail.com>
2025-04-09 22:26:51 +0100

    Try and fix Windows

--- pypi_timemachine/tests/test_basic.py
+++ pypi_timemachine/tests/test_basic.py
@@ -4,12 +4,11 @@ import time
 import contextlib
 import subprocess
 
-@contextlib.contextmanager
-def timemachine(date,  port=8100):
 
-    process = subprocess.Popen(
-        ['pypi-timemachine',
-    date, '--port', str(port)])
+@contextlib.contextmanager
+def timemachine(date, port=8100):
+
+    process = subprocess.Popen(["pypi-timemachine", date, "--port", str(port)])
 
     try:
         time.sleep(2)
@@ -17,6 +16,7 @@ def timemachine(date,  port=8100):
     finally:
         process.terminate()
         process.wait()
+
 
 BASIC_EXPECTED = """
 astropy==7.0.0
@@ -27,9 +27,25 @@ pyerfa==2.0.1.5
 PyYAML==6.0.2
 """.strip()
 
+
 def test_basic(tmpdir):
-    subprocess.check_output([sys.executable, '-m', 'venv', tmpdir])
-    with timemachine('2024-12-03T22:12:33'):
-        subprocess.check_output([os.path.join(tmpdir, 'bin', 'python'), '-m', 'pip', 'install', '-i', f'http://localhost:8100', 'astropy'])
-    freeze_output = subprocess.check_output([os.path.join(tmpdir, 'bin', 'python'), '-m', 'pip', 'freeze'])
-    assert freeze_output.decode('utf-8').strip() == BASIC_EXPECTED
+    subprocess.check_output([sys.executable, "-m", "venv", tmpdir])
+    python_executable = os.path.join(
+        tmpdir,
+        "Scripts" if os.name == "nt" else "bin",
+        "python",
+    )
+    with timemachine("2024-12-03T22:12:33"):
+        subprocess.check_output(
+            [
+                python_executable,
+                "-m",
+                "pip",
+                "install",
+                "-i",
+                f"http://localhost:8100",
+                "astropy",
+            ]
+        )
+    freeze_output = subprocess.check_output([python_executable, "-m", "pip", "freeze"])
+    assert freeze_output.decode("utf-8").strip() == BASIC_EXPECTED
