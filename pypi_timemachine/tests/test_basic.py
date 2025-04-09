22a019eab2c71e63d0cf66f6511fc76932e9d542
Thomas Robitaille <thomas.robitaille@gmail.com>
2025-04-09 21:59:23 +0100

    Fixed test

--- pypi_timemachine/tests/test_basic.py
+++ pypi_timemachine/tests/test_basic.py
@@ -19,18 +19,17 @@ def timemachine(date,  port=8100):
         process.wait()
 
 BASIC_EXPECTED = """
-astropy==7.0.1
-astropy-iers-data==0.2025.4.7.0.35.30
-numpy==2.2.4
+astropy==7.0.0
+astropy-iers-data==0.2024.12.2.0.35.34
+numpy==2.1.3
 packaging==24.2
 pyerfa==2.0.1.5
 PyYAML==6.0.2
-"""
+""".strip()
 
 def test_basic(tmpdir):
     subprocess.check_output([sys.executable, '-m', 'venv', tmpdir])
-    with timemachine('2023-02-03T22:12:33'):
+    with timemachine('2024-12-03T22:12:33'):
         subprocess.check_output([os.path.join(tmpdir, 'bin', 'python'), '-m', 'pip', 'install', '-i', f'http://localhost:8100', 'astropy'])
     freeze_output = subprocess.check_output([os.path.join(tmpdir, 'bin', 'python'), '-m', 'pip', 'freeze'])
-    print(freeze_output.decode('utf-8'))
-    assert freeze_output == BASIC_EXPECTED
+    assert freeze_output.decode('utf-8').strip() == BASIC_EXPECTED
